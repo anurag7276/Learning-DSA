0116e1a524c70125c99157a481cfbc22a6711cd9
anurag7276 <161753889+anurag7276@users.noreply.github.com>
2024-09-21 16:27:58 +0530

    Create generateParenthesis.cpp

---
+++ Recursion/leetcode/generateParenthesis.cpp
@@ -0,0 +1,49 @@
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+ 
+
+Example 1:
+
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+
+
+class Solution {
+public:
+    
+    void parenth(int n, int left,int right,vector<string>&ans,string &temp)
+    {
+          // base condition
+          if(left+right==2*n)
+          {
+            ans.push_back(temp);
+            return;
+          }
+
+
+          // left add parenthesis
+
+          if(left<n)
+          {
+            temp.push_back('(');
+            parenth(n,left+1,right,ans,temp);
+            temp.pop_back();
+
+          }
+          // right add
+          if(right<left)
+          {
+            temp.push_back(')');
+            parenth(n,left,right+1,ans,temp);
+            temp.pop_back();
+          }
+    }
+
+    vector<string> generateParenthesis(int n) {
+        
+        vector<string> ans;
+        string temp;
+        parenth(n,0,0,ans,temp);
+        return ans;
+    }
+};
